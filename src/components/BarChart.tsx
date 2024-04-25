5bd935afa7c0b9e879dbcde9c878ff61725755b9
Rishith25 <kamishetty.rishith@gmail.com>
2024-04-25 21:38:40 +0530

    chartjs corrected

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -55,32 +55,6 @@ const BarChart: React.FC<{ chartData: any }> = ({ chartData }) => {
             borderColor: "#4CAF50",
           },
         },
-        scales: {
-          x: {
-            grid: {
-              display: false, // Hide vertical grid lines
-            },
-            ticks: {
-              color: "#777", // X-axis label color
-              font: {
-                size: 12, // Font size for X-axis labels
-                weight: "bold", // Font weight for X-axis labels
-              },
-            },
-          },
-          y: {
-            grid: {
-              color: "#f0f0f0", // Color of horizontal grid lines
-            },
-            ticks: {
-              color: "#777", // Y-axis label color
-              font: {
-                size: 12, // Font size for Y-axis labels
-                weight: "bold", // Font weight for Y-axis labels
-              },
-            },
-          },
-        },
         maintainAspectRatio: true, // Maintain aspect ratio
         responsive: true, // Make the chart responsive
         aspectRatio: 3, // Set aspect ratio for width and height
